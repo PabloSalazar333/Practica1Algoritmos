c877b0b7d5531b8b7790efba2232c3afd525462f
PabloSalazar333 <p.e.salazarmarquez@gmail.com>
2019-10-10 13:17:43 -0500

    This is my third commit. Add private id and private name

--- src/com/practica1algoritmos/app/HolaMundo.java
+++ src/com/practica1algoritmos/app/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hola Munditu!!!");
-		
+		System.out.println("Esta es otra linea de codigo");
 
 	}
 
--- src/com/practica1algoritmos/model/Dog.java
+++ src/com/practica1algoritmos/model/Dog.java
@@ -1,5 +1,7 @@
 package com.practica1algoritmos.model;
 
 public class Dog {
+	private int id;
+	private int name;
 
 }
